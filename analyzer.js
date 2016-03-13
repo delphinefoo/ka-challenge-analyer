22cc1999253121ce4418ea2f2f549e765d9f717a
Delphine Foo-Matkin <delphinef@gmail.com>
2016-03-12 17:16:53 -0800

    Added success message and message styling.

--- analyzer.js
+++ analyzer.js
@@ -4,16 +4,30 @@ $(function() {
   tester.required('if');
   tester.banned('while');
   tester.banned('variable');
+
   $('#answer').on('keyup', function() {
     var answer = $('#answer').val();
     var whitelistError = tester.findRequired(answer);
     var blacklistError = tester.findBanned(answer);
-    $('#error ul').empty();
-    if (whitelistError !== null) {
-      $('#error ul').append('<li>' + whitelistError + '</li>');
+
+    $('#message ul').empty();
+
+    if (whitelistError || blacklistError) {
+      $('#message').addClass('error');
+      $('#message').removeClass('success');
+    } else {
+      $('#message').addClass('success');
+      $('#message').removeClass('error');
+      $('#message ul').append('<li>Good job!</li>');
     }
+
+
+    if (whitelistError !== null) {
+      $('#message ul').append('<li>' + whitelistError + '</li>');
+    }
+
     if (blacklistError !== null) {
-      $('#error ul').append('<li>' + blacklistError + '</li>');
+      $('#message ul').append('<li>' + blacklistError + '</li>');
     }
   });
 
